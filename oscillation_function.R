a6e9193c338bf1aef36c3287cd3a1517cced22d4
annaCunningham <101744716+annaCunningham@users.noreply.github.com>
2022-08-22 21:44:46 +0100

    Update oscillation_function.R

--- oscillation_function.R
+++ oscillation_function.R
@@ -1,8 +1,8 @@
 # Function that generates the values for a piece-wise signature
 oscillation_function <- function(time, oscillation_number, period, spike_width, max,min){
   zero_second = 0
-  zero_oscillation = ifelse(time>=(zero_second) & time <(spike_width), max, 
-                            ifelse(time>(spike_width) & time < (period),min, min))
+  zero_oscillation = ifelse(time >= (zero_second) & time <(spike_width), max, 
+                            ifelse(time > (spike_width) & time < (period), min, min))
   all_oscillations = zero_oscillation
   
   if(oscillation_number>1){
