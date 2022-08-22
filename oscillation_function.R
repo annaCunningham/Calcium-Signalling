f82a1426bd3134847dacc90a2e60f9142a9e711e
annaCunningham <101744716+annaCunningham@users.noreply.github.com>
2022-08-22 21:43:25 +0100

    Create oscillation_function.R
    Function that creates co-ordinate values for piece-wise signature.

---
+++ oscillation_function.R
@@ -0,0 +1,17 @@
+# Function that generates the values for a piece-wise signature
+oscillation_function <- function(time, oscillation_number, period, spike_width, max,min){
+  zero_second = 0
+  zero_oscillation = ifelse(time>=(zero_second) & time <(spike_width), max, 
+                            ifelse(time>(spike_width) & time < (period),min, min))
+  all_oscillations = zero_oscillation
+  
+  if(oscillation_number>1){
+    for(count in 1:(oscillation_number-1)){
+      all_oscillations <- append(all_oscillations, zero_oscillation)
+    } 
+  }
+  else{
+    all_oscillations=all_oscillations
+    }
+  return(all_oscillations)
+}
