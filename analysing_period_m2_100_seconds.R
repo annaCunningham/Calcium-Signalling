e77f5d43c5adce97955ffcbc81baa36c6cc2c348
annaCunningham <101744716+annaCunningham@users.noreply.github.com>
2022-08-24 19:46:14 +0100

    Create analysing_period_m2_100_seconds.R
    Analysing period duration using method 2 for a signature with oscillations with 100 second periods.

---
+++ analysing_period_m2_100_seconds.R
@@ -0,0 +1,58 @@
+# Examining changing period. 
+# Period duration of 100 seconds and a total of 10 oscillations. 
+
+## CREATING THE PIECEWISE OSCILLATION.
+oscillation_number = 10 
+period = 100          
+time = c(0:(period-1))
+spike_width = 14
+max = 0.52
+min = 0.1
+fig_18_df = data.frame(x = c(0,c(1:(oscillation_number*period))), 
+                       S =  c(min,oscillation_function(time, oscillation_number, period, spike_width, max, min)))
+# Plot piece-wise oscillation.
+figure_18 = graphing_function(fig_18_df, fig_18_df$x, fig_18_df$S, "Calcium Oscillation", "Time", expression(paste("[Ca"^"2+"*"]", mu, "M")))+
+  geom_step(colour="black") + geom_hline(yintercept=mean(fig_18_df$S),color="gray0", 
+                                         linetype="dashed") +
+  annotate('text', x=(oscillation_number*(period+0.2)), y=mean(fig_18_df$S)+0.02, label="bar(S) ", parse=TRUE, size=4) 
+figure_18
+
+## CREATING PLOT FOR Y ACTIVITY.
+fig_19_df = fig_18_df 
+fig_19_df$Y_Values = as.numeric(unlist(y_activation_function(fig_19_df)))
+end_t2 = tail(fig_19_df$x, 1)
+start_t1 = (end_t2-period)
+average_y = calculate_average_function(start_t1, end_t2, fig_19_df$Y_Values)
+
+# Plot changes in Y activity.
+figure_19 = graphing_function(fig_19_df, fig_19_df$x, fig_19_df$Y_Values, "Calcium-dependent kinase",
+                              "Time (s)","CDPK") + geom_line(colour="blue4") + 
+  geom_segment(aes(x=0, y=1, xend=1000, yend=1), colour="black",
+               arrow=arrow(length=unit(0.1, "cm"), ends="both")) +
+  annotate("text", x=500 , y=1.05, label="Transient", parse=TRUE, size=3.5)
+figure_19
+
+## CREATING PLOT FOR X ACTIVITY.
+fig_19_df$X_Values = as.numeric(unlist(x_activation_function(fig_19_df)))
+end_t2=tail(fig_19_df$x, 1)
+start_t1=(end_t2-period)
+dataframe=fig_19_df$X_Values
+average_x = calculate_average_function(start_t1, end_t2, dataframe)
+fig_20_df = fig_19_df[-1,] 
+
+# Plot changes in X activity.
+figure_20 = graphing_function(fig_20_df, fig_20_df$x, fig_20_df$X_Values, "Target Transcription Factor ",
+                              "Time (s)", "TF") + geom_path(colour="blue4") + 
+  geom_segment(aes(x=0, y=0.33, xend=1000, yend=0.33), colour="black",
+               arrow=arrow(length=unit(0.1, "cm"), ends="both")) +
+  annotate("text", x=500 , y=0.34, label="Transient", parse=TRUE, size=3.3)
+figure_20
+
+## CALCULATING CHANGES IN [mRNA]
+fig_18_mRNA_df = fig_20_df
+fig_18_mRNA_df$X_concentration = (fig_18_mRNA_df$X_Values *1) #assume total (x) concentration is 1
+#add mRNA concentration column 
+X_conc_value_temp = fig_18_mRNA_df$X_concentration
+fig_18_mRNA_df$mRNA_concentration  = mRNA_concentration_function(X_conc_value = X_conc_value_temp)
+#calculating and plotting fold change 
+fig_18_mRNA_df$fold_change = fig_18_mRNA_df$mRNA_concentration/SS_mRNA_0.16
