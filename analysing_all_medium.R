bca5bc15dd1cd9d72128445aeafa9a708f1b4590
annaCunningham <101744716+annaCunningham@users.noreply.github.com>
2022-08-27 20:18:20 +0100

    Update analysing_all_medium.R

--- analysing_all_medium.R
+++ analysing_all_medium.R
@@ -67,5 +67,3 @@ X_conc_value_temp = fig_44_mRNA_df$X_concentration
 fig_44_mRNA_df$mRNA_concentration  = mRNA_concentration_function(X_conc_value = X_conc_value_temp)
 #calculating and plotting fold change 
 fig_44_mRNA_df$fold_change = fig_44_mRNA_df$mRNA_concentration/SS_mRNA_0.3
-
-
