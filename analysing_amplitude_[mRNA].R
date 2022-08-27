aca34347a375126d2f4df4fe0a310ad634fc6b22
annaCunningham <101744716+annaCunningham@users.noreply.github.com>
2022-08-27 20:12:25 +0100

    Update analysing_amplitude_[mRNA].R

--- analysing_amplitude_[mRNA].R
+++ analysing_amplitude_[mRNA].R
@@ -15,7 +15,7 @@ fig_amplitude_mRNA_conc = ggplot(data=amplitude_mRNA, mapping = aes(x=time)) +
   geom_line(mapping = aes(y=amplitude0.7, color="0.7  ")) +
   geom_line(mapping = aes(y=amplitude1.2, color="1.2  "))  +
   scale_color_manual(values = c("0.2  "="darkslategray","0.7  "="darkslategray4", "1.2  "="darkslategray3")) +
-  xlab("Time (s)") + ylab(expression(paste("[mRNA] ", mu, "M")))+
+  xlab("Time (s)") + ylab(expression(paste("[mRNA] ", mu, "M"))) +
   theme(plot.title=element_text(size=10, face="bold"),axis.title.x=element_text(size=9.5),
         axis.title.y=element_text(size=9.5), legend.position = "top", legend.title = element_blank()) 
 
@@ -24,9 +24,8 @@ fig_amplitude_mRNA_fold_change = ggplot(data=amplitude_mRNA_fold_change, mapping
   geom_line(mapping = aes(y=amplitude0.7, color="0.7  ")) +
   geom_line(mapping = aes(y=amplitude1.2, color="1.2  ")) +
   scale_color_manual(values = c("0.2  "="darkslategray","0.7  "="darkslategray4", "1.2  "="darkslategray3")) +
-  xlab("Time (s)") + ylab(expression(paste("[mRNA]-fold change")))+
+  xlab("Time (s)") + ylab(expression(paste("[mRNA]-fold change"))) +
   theme(plot.title=element_text(size=10, face="bold"),axis.title.x=element_text(size=9.5),
         axis.title.y=element_text(size=9.5),legend.position = "top",legend.title = element_blank()) 
 
 grid.arrange(fig_amplitude_mRNA_conc, fig_amplitude_mRNA_fold_change, ncol=2)
-
